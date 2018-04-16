d620668ab1ee1985e1b821f5fe330d064a369185
zhanghl-k <zhanghl-k@glodon.com>
2018-04-16 18:28:22 +0800

    await

--- test/es.spec.ts
+++ test/es.spec.ts
@@ -1,19 +1,20 @@
 import { expect } from 'chai';
+// import * as sinon from 'sinon'
 describe(`The Float32Array typed array represents an array of 32-bit floating point numbers (corresponding to the C float data type) in the platform byte order.`, () => {
     beforeAll: {
         process.on('unhandledRejection', function (err, promise) {
             console.log('Unhandled rejection (promise: ', promise, ', reason: ', err, ').');
         });
     }
-    describe(`https://wanago.io/2018/04/16/explaining-async-await-creating-dummy-promises/`, (done) => {
-        it(`beginners try to do something like that`, () => {
+    describe(`https://wanago.io/2018/04/16/explaining-async-await-creating-dummy-promises/`, () => {
+        it(`beginners try to do something like that`, (done) => {
             function fetchUsers() {
                 let users;
                 new Promise(resolve => {
                     setTimeout(() => {
                         users = 'setTimeout'
                         resolve(users)
-                    })
+                    }, 500)
                 }).then(data => {
                     expect(data).to.equal('setTimeout')
                     done()
@@ -21,6 +22,76 @@ describe(`The Float32Array typed array represents an array of 32-bit floating po
                 expect(users).to.equal(); // undefined
             }
             fetchUsers()
+        });
+        it(`write asynchronous code that looks synchronous`, () => {
+            async function fetchUsers() {
+                const usersResponse = await new Promise(resolve => setTimeout(() => resolve(42), 500));
+                const usersData = await new Promise(resolve => setTimeout(() => resolve(usersResponse + 10), 500));
+                expect(usersData).to.equal(52);
+            }
+            fetchUsers()
+        });
+        it(`With async/await, a try catch block makes a big return`, () => {
+            async function fetchUsers() {
+                try {
+                    const usersResponse = await fetch(`${apiUrl}/users`);
+                    const usersData = await usersResponse.json();
+                } catch (e) {
+                    expect(e.message).to.equal("fetch is not defined");
+                }
+            }
+            fetchUsers()
+        });
+        it(`Companies are fetched only after the users were fetched`, () => {
+            async function fetchUsers() {
+                const start = new Date().getTime();
+                const Companies = await new Promise(resolve => setTimeout(() => resolve(10), 500));
+                const users = await new Promise(resolve => setTimeout(() => resolve(42), 800));
+                const end = new Date().getTime();
+                expect(end - start).to.greaterThan(500 + 800 - 1);
+            }
+            fetchUsers()
+        });
+        it(`Promise.all runs all requests in parallel and returns a new promise to whom we can wait for`, () => {
+            async function fetchUsers() {
+                const start = new Date().getTime();
+                const Companies = await Promise.all([new Promise(resolve => setTimeout(() => resolve(10), 500)),
+                new Promise(resolve => setTimeout(() => resolve(42), 800))]);
+                const end = new Date().getTime();
+                expect(end - start).to.lessThan(500 + 800);
+            }
+            fetchUsers()
+        });
+        it(`await calls the then method.`, () => {
+            const usersPromise = new Promise(resolve => setTimeout(() => resolve(10), 500))
+            expect(usersPromise.then).to.be.a('function') // a function here
+
+            usersPromise.then(data => 42);
+            async function fetchUsers() {
+
+                const usersResponse = await usersPromise;
+                usersPromise.then(data => {
+
+                    expect(data).to.equal(10)
+                })
+            }
+            fetchUsers()
+        });
+        it(` this points to the promise itself, just like it would if a then method was simply called on it.`, () => {
+            function customThen(resolve, reject) {
+                resolve(this.response);
+            }
+
+            const dummyPromise = {
+                response: 'that is the question',
+                then: customThen
+            };
+
+            dummyPromise.then(response => {
+                expect(response).to.equal('that is the question');
+                // To be, or not to be 
+                // that is the question
+            });
         });
     });
     it(`Different ways to create a Float32Array:`, () => {
