f4badfe7e6a791223a8d76f08cc326bfed14efa2
qcgm1978 <zhanghl@strongunion.com.cn>
2018-04-19 22:37:57 +0800

    inspect

--- test/jest.spec.ts
+++ test/jest.spec.ts
@@ -1,3 +1,15 @@
 test('the best flavor is grapefruit', () => {
     expect('grapefruit').toBe('grapefruit');
 });
+describe(`http://2ality.com/2018/04/type-notation-typescript.html`, () => {
+    beforeAll: {
+        process.on('unhandledRejection', function (err, promise) {
+            console.error('Unhandled rejection (promise: ', promise, ', reason: ', err, ').');
+        });
+    }
+    it(`If you store a two-dimensional point in an Array then you are using that Array as a tuple.`, () => {
+        let point: [number, number] = [7, 5];
+        expect(point).toEqual([7, 5])
+        // const point1 = Object.entries({ a: 1, b: 2 })
+    });
+});
